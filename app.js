fb1eb3d8acee9c1e6c904fcbc49024d82daa727e
Ygshberwal26 <yogeshberwal266@gmail.com>
2024-03-21 10:26:42 +0530

    L2: fav books

--- app.js
+++ app.js
@@ -2,10 +2,41 @@ const app=Vue.createApp({
     //template: '<h2> I am Template</h2>'
     data(){
         return{
-            showBooks: true,
-            title: "The Final Empire",
-            author:"Branda Anderson",
-            age:40
+            showMyBooks:true,
+            x:0,
+            y:0,
+            url:"https://www.google.com", 
+            myBooks:[
+                {title:"Murder in Orient Express" ,author:"Agatha Christie",img:'assets/m.jpg', isFav:true},
+                {title:"Atomic Habits" ,author:"James Clear",img:'assets/a.jpg', isFav:false},
+                {title:"2 States and half girlfriend" ,author:"Chetan Bhagat",img:'assets/s.jpg', isFav:true},
+            ],
+            books:[
+                {author:'Agatha Christie', title:["Then there were none",
+                                          "Death on the Nile ",
+                                          "Murder on the Orient Express",
+                                          "The Murder on the Links ",
+                                          "The ABC murder"
+                                        ],showBooks: false,
+                                        img:'assets/1.jpg'
+                                    },
+                {author:'Colleen Hoover', title:["It Ends with Us ",
+                                                 "November 9 ",
+                                                 "Ugly Love ",
+                                                 "Maybe Someday ",
+                                                 "Losing Hope "
+                                                ],showBooks: false,
+                                                img:'assets/2.jpg'
+                                            },
+                {author:"Chetan Bhagat", title:["Five point someone",
+                                                "The girl in room 105",
+                                                "The three mistakes of my life",
+                                                "2 States",
+                                                "Half girlfriend"
+                                                ],showBooks: false,
+                                                img:'assets/3.jpg'
+                                            }
+            ]
          }
     },
     methods:{
@@ -13,9 +44,31 @@ const app=Vue.createApp({
             // this.title="Words of Radaince"
             this.title=title
         },
-        toggleShowBooks(){
-            this.showBooks=!this.showBooks
+        toggleShowBooks(author){
+            author.showBooks=!author.showBooks
+        },
+        toggleMyBooks(){
+            this.showMyBooks=!this.showMyBooks
+        },
+        toggleFav(book){
+            book.isFav=!book.isFav
+        },
+        handleEvent(e, n){
+            console.log(e,e.type)
+            if (n){
+                console.log(n)
+            }
+        },
+        handleMouseMove(e){
+            this.x=e.offsetX
+            this.y=e.offsetY
         }
+    },
+    computed:{
+        filteredBooks(){
+            return this.myBooks.filter((book)=>book.isFav)
+        }
+        
     }
 })
 
